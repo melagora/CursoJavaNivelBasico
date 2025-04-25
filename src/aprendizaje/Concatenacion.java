362bcb1d7642bc11b669ed47b04dc5f96b7dfd27
Melvin González <54681730+melagora@users.noreply.github.com>
2025-04-24 19:22:28 -0600

    Concatenacion de variables

---
+++ src/aprendizaje/Concatenacion.java
@@ -0,0 +1,11 @@
+package aprendizaje;
+
+public class Concatenacion {
+    public static void main(String[] args) {
+        String nombre = "Juan";
+        String apellido = "Pérez";
+        String nombreCompleto = nombre + " " + apellido;
+
+        System.out.println("Nombre completo: " + nombreCompleto);
+    }
+}
