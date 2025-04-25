1a61e974ba6e18bd22bb81fa011af699336a0d24
Melvin González <54681730+melagora@users.noreply.github.com>
2025-04-25 11:09:36 -0600

    Estructura condicional FOR

---
+++ src/aprendizaje/EstructuraCondicional_FOR.java
@@ -0,0 +1,19 @@
+package aprendizaje;
+
+public class EstructuraCondicional_FOR {
+    public static void main(String[] args) {
+        // Estructura de control for
+        // Se utiliza para iterar sobre un rango de valores o una colección de
+        // elementos.
+        // La sintaxis básica es:
+        // for (inicialización; condición; incremento) {
+        // // Código a ejecutar en cada iteración
+        // }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("El valor de i es: " + i);
+        }
+        // El bucle for se utiliza comúnmente cuando se conoce el número exacto de
+        // iteraciones que se desean realizar.
+    }
+}
