ced72d3193b3d67bf61369993b470ae6a4834235
Melvin González <54681730+melagora@users.noreply.github.com>
2025-04-24 21:11:52 -0600

    Estructura IF basica

---
+++ src/aprendizaje/EstructuraCondicional_IF.java
@@ -0,0 +1,22 @@
+package aprendizaje;
+
+import java.util.Scanner;
+
+public class EstructuraCondicional_IF {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+        if (numero > 0) {
+            System.out.println("El número es positivo.");
+        } else if (numero < 0) {
+            System.out.println("El número es negativo.");
+        } else {
+            System.out.println("El número es cero.");
+        }
+
+        scanner.close();
+    }
+}
