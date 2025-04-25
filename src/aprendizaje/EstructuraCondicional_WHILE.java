31ab798c11e381e5c2e4a702c65d8cce2077e775
Melvin González <54681730+melagora@users.noreply.github.com>
2025-04-25 11:05:48 -0600

    Estructura condicional while

---
+++ src/aprendizaje/EstructuraCondicional_WHILE.java
@@ -0,0 +1,12 @@
+package aprendizaje;
+
+public class EstructuraCondicional_WHILE {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println("El valor de i es: " + i);
+            i++;
+        }
+    }
+}
