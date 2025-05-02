f80340807c84189bd6cb98de7b329b8ee28ba263
Melvin González <54681730+melagora@users.noreply.github.com>
2025-05-01 19:04:56 -0600

    Metodos pasando parametros

---
+++ src/aprendizaje/EjemplosMetodos2.java
@@ -0,0 +1,13 @@
+package aprendizaje;
+
+public class EjemplosMetodos2 {
+
+    static void Mensaje(String cTexto, String cTexto2) {
+        System.out.println(cTexto);
+        System.err.println(cTexto2);
+    }
+
+    public static void main(String[] args) {
+        Mensaje("Hola Mundo desde un método no estático", "Este es un ejemplo de un método no estático en Java.");
+    }
+}
