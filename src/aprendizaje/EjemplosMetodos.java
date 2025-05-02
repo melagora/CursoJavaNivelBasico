d799349aaf525f3ca00e9033872d35c63bc60f7f
Melvin González <54681730+melagora@users.noreply.github.com>
2025-05-01 19:00:44 -0600

    Introducción a los metodos

---
+++ src/aprendizaje/EjemplosMetodos.java
@@ -0,0 +1,13 @@
+package aprendizaje;
+
+public class EjemplosMetodos {
+
+    static void Mensaje() {
+        System.out.println("Hola Mundo desde un método estático");
+        System.out.println("Este es un ejemplo de un método estático en Java.");
+    }
+
+    public static void main(String[] args) {
+        Mensaje();
+    }
+}
